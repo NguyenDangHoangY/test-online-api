62d6a4dfa17781f5ce41f96d4a6e5399e7eedb9f
y nguyen <y.nguyen@orientsoftware.com>
2022-10-20 15:06:03 +0700

    add unit test for question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
     has_many :answers, dependent: :destroy
-    validates(:content, presence: true, length: {maximum: 50})
+    validates(:content, presence: true, length: {maximum: 200})
     validates(:point, presence: true)
     validates(:correctAnswer, presence: true)
     validates(:answer, presence: true, allow_nil:true)
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -8,4 +8,24 @@ class QuestionTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @question.valid?
   end
+
+  test "content should be present" do
+    @question.content = "  "
+    assert_not @question.valid?
+  end
+
+  test "content should not be too long" do 
+    @question.content = "a"*201
+    assert_not @question.valid?
+  end
+
+  test "point should be present" do
+    @question.point = " "
+    assert_not @question.valid?
+  end
+
+  test "correct answer should be present" do
+    @question.correctAnswer = " "
+    assert_not @question.valid?
+  end
 end
