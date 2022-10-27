cbd0550d5b7fec38c32afb6b1411ef3f1a6cfdb1
y nguyen <y.nguyen@orientsoftware.com>
2022-10-27 17:14:42 +0700

    success save user point to db

---
+++ app/graphql/mutations/update_user.rb
@@ -0,0 +1,33 @@
+module Mutations
+    class UpdateUser < BaseMutation
+      # TODO: define return fields
+      # field :post, Types::PostType, null: false
+  
+      # TODO: define arguments
+      # argument :name, String, required: true
+  
+      # TODO: define resolve method
+      # def resolve(name:)
+      #   { post: ... }
+      # end
+      
+      field :user, Types::UserType, null: false
+
+      argument :id, ID, required: true  
+      argument :name, String, required: false
+      argument :email, String, required: false
+      argument :password, String, required: false
+      argument :point, Float, required: false
+      
+      def resolve(id:, **arg)
+        user = User.find(id)
+        # user = User.new(arg)
+        if user.update(arg)
+          {
+            user: user
+          }
+        end
+      end
+    end
+  end
+  
